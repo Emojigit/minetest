93e182e21d588dc2814c70cbafdff66deebd0a85
1F616EMO <root@1f616emo.xyz>
2024-07-02 06:12:46 +0800

    Ignore warning about getVertexTypeDescription reaching the unreachable end

--- irr/src/OpenGL/Driver.cpp
+++ irr/src/OpenGL/Driver.cpp
@@ -99,6 +99,8 @@ static const VertexType vtTangents = {
 };
 
 #pragma GCC diagnostic pop
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
 
 static const VertexType &getVertexTypeDescription(E_VERTEX_TYPE type)
 {
@@ -113,6 +115,8 @@ static const VertexType &getVertexTypeDescription(E_VERTEX_TYPE type)
 		IRR_CODE_UNREACHABLE();
 	}
 }
+
+#pragma GCC diagnostic pop
 
 static const VertexType vt2DImage = {
 		sizeof(S3DVertex),
