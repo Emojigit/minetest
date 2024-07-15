405659843b3c2c10f06e78d1217961eeef11e80f
1F616EMO <root@1f616emo.xyz>
2024-07-15 19:29:14 +0800

    Oppsie, forgot to return true

--- builtin/game/chat.lua
+++ builtin/game/chat.lua
@@ -1331,6 +1331,7 @@ core.register_chatcommand("clearobjects", {
 				core.clear_objects(options)
 				core.log("action", "Object clearing done.")
 				core.chat_send_all("*** "..S("Cleared all objects."))
+				return true
 			end
 			return true, S("Operation canceled.")
 		end)
