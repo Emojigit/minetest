162d685e22cbf8aa068929309382f6c831b881d9
1F616EMO <root@1f616emo.xyz>
2024-07-15 21:42:12 +0800

    Revert "Merge branch 'fork-20240715-chat-confirm' into fork-master"
    This reverts commit 8f92721b895b3f83d8c307023b46cca085e51a3a, reversing
    changes made to e13b3e40c488ad6c908759b751a888f3ef4112f9.

--- builtin/game/chat.lua
+++ builtin/game/chat.lua
@@ -1331,7 +1331,6 @@ core.register_chatcommand("clearobjects", {
 				core.clear_objects(options)
 				core.log("action", "Object clearing done.")
 				core.chat_send_all("*** "..S("Cleared all objects."))
-				return true
 			end
 			return true, S("Operation canceled.")
 		end)
