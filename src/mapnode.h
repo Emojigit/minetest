8007d87f3ba3d047302c7742c36f56c731dbd7dc
1F616EMO <root@1f616emo.xyz>
2024-07-16 19:28:20 +0800

    Revert "Merge branch 'fork-20240716-4dir-560-compact' into fork-master"
    This reverts commit 28695aaaf19134ee9499cfae444353c1c0c1d9b0, reversing
    changes made to b47f73c8dba16f84d36a3d63133c9fcce1b4d8cf.

--- src/mapnode.h
+++ src/mapnode.h
@@ -318,9 +318,7 @@ struct alignas(u32) MapNode
 	//   compressed = true to zlib-compress output
 	static Buffer<u8> serializeBulk(int version,
 			const MapNode *nodes, u32 nodecount,
-			u8 content_width, u8 params_width,
-			u16 protocol_version,
-			const NodeDefManager *nodemgr);
+			u8 content_width, u8 params_width);
 	static void deSerializeBulk(std::istream &is, int version,
 			MapNode *nodes, u32 nodecount,
 			u8 content_width, u8 params_width);
